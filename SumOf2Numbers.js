41ea175d9c9f18405cf47c35c8954c9d0811abe4
Srinivas Naren Vemgal <84729791+SrinivasNaren@users.noreply.github.com>
2026-02-17 09:45:58 +0530

    Create SumOf2Numbers.js

---
+++ SumOf2Numbers.js
@@ -0,0 +1,13 @@
+/*write a function that takes 2 numbers as input and print their sum
+# input : 2 nums a & b
+# sum = a+b
+# print the sum on console */
+
+function sum(a,b){
+  const sum = a+b;
+  console.log('sum is',sum):
+}
+
+/*testing*/
+sum(67,76);
+sum(-23,89);
